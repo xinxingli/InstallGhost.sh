52dbf35e24f847b16d6f08c1219a5e255f6a403f
li xinxing <lxx19950227@163.com>
2016-06-10 14:04:17 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,91 @@
+var path = require('path'),
+    config;
+config = {
+
+    production: {
+        url: 'http://EasyGhost',
+        mail: {},
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(__dirname, '/content/data/ghost.db')
+            },
+            debug: false
+        },      
+        server: {
+            host: '0.0.0.0',
+            port: '2368'
+        },
+        storage: {
+            provider: 'local-file-store'
+        }
+    },
+    development: {
+        url: 'http://localhost:2368',
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(__dirname, '/content/data/ghost-dev.db')
+            },
+            debug: false
+        },
+        server: {
+            host: '127.0.0.1',
+            port: '2368'
+        },
+        paths: {
+            contentPath: path.join(__dirname, '/content/')
+        }
+    },
+    testing: {
+        url: 'http://127.0.0.1:2369',
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(__dirname, '/content/data/ghost-test.db')
+            }
+        },
+        server: {
+            host: '127.0.0.1',
+            port: '2369'
+        },
+        logging: false
+    },
+    'testing-mysql': {
+        url: 'http://127.0.0.1:2369',
+        database: {
+            client: 'mysql',
+            connection: {
+                host     : '127.0.0.1',
+                user     : 'root',
+                password : '',
+                database : 'ghost_testing',
+                charset  : 'utf8'
+            }
+        },
+        server: {
+            host: '127.0.0.1',
+            port: '2369'
+        },
+        logging: false
+    },
+    'testing-pg': {
+        url: 'http://127.0.0.1:2369',
+        database: {
+            client: 'pg',
+            connection: {
+                host     : '127.0.0.1',
+                user     : 'postgres',
+                password : '',
+                database : 'ghost_testing',
+                charset  : 'utf8'
+            }
+        },
+        server: {
+            host: '127.0.0.1',
+            port: '2369'
+        },
+        logging: false
+    }
+};
+module.exports = config;
